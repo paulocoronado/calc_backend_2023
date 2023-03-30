30a9deaa07d9647e21810b28be8f88899847c3c5
Paulo Coronado <paulocoronado@udistrital.edu.co>
2023-03-30 08:03:48 -0500

    v 0.0.3

--- index.js
+++ index.js
@@ -6,19 +6,30 @@ const app= express();
 
 app.use(express.json());
 
+
+app.use( function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST");
+    res.header("Access-Control-Allow-Headers", "Content-type");
+    next();
+}
+);
+
 //Definir los entry point de la API
 //Definir la ruta (la url) en dónde va a responder nuestra API
 // http://localhost:3000/
 
 
-app.get(
+app.post(
     '/api/sumar',
     //Se requieren dos objetos: uno representando la petición
     //un objeto representando la respuesta
     (req, res)=>{
         //To Do: Aquí va el procesamiento de la petición a esta ruta
         console.log("Alguien está conectándose a esta ruta!!");        
-        res.json(req.body);
+        const {numero_1, numero_2}= req.body;
+        const resultado=parseFloat(numero_1)+parseFloat(numero_2);        
+        res.json(resultado);
     }
 );
 
@@ -55,8 +66,8 @@ app.post(
 
 //3. Crear un servicio para escuchar peticiones
 app.listen(
-    3001,
+    3000,
     ()=>{
-      console.log("Servidor ejecutándose en el puerto 3001");
+      console.log("Servidor ejecutándose en el puerto 3000");
     }
 );
